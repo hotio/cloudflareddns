654b01a48b199961155dd301802143921e1dcd5f
hotio <hotio@protonmail.com>
2019-08-14 15:45:19 +0200

    improve performance when updating lots of domains and log connection status to influxdb

--- root/app/cloudflare-ddns.sh
+++ root/app/cloudflare-ddns.sh
@@ -19,9 +19,65 @@ read -r -a cfhost <<< "${CF_HOSTS}"
 read -r -a cftype <<< "${CF_RECORDTYPES}"
 IFS="${DEFAULTIFS}"
 
-###############
-## UPDATE IP ##
-###############
+######################
+## CHECK FOR NEW IP ##
+######################
+
+case "${DETECTION_MODE}" in
+    dig-google.com)
+        newipv4=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
+        newipv6=$(dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
+        ;;
+    dig-opendns.com)
+        newipv4=$(dig -4 A +short myip.opendns.com @resolver1.opendns.com)
+        newipv6=$(dig -6 AAAA +short myip.opendns.com @resolver1.opendns.com)
+        ;;
+    curl-icanhazip.com)
+        newipv4=$(curl -s -4 icanhazip.com)
+        newipv6=$(curl -s -6 icanhazip.com)
+        ;;
+    curl-wtfismyip.com)
+        newipv4=$(curl -s -4 wtfismyip.com/text)
+        newipv6=$(curl -s -6 wtfismyip.com/text)
+        ;;
+    curl-showmyip.ca)
+        newipv4=$(curl -s -4 showmyip.ca/ip.php)
+        newipv6=$(curl -s -6 showmyip.ca/ip.php)
+        ;;
+    curl-da.gd)
+        newipv4=$(curl -s -4 da.gd/ip)
+        newipv6=$(curl -s -6 da.gd/ip)
+        ;;
+    curl-seeip.org)
+        newipv4=$(curl -s -4 ip.seeip.org)
+        newipv6=$(curl -s -6 ip.seeip.org)
+        ;;
+esac
+
+regexv4='^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'
+regexv6='^([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}$'
+
+#######################################
+## LOG CONNECTION STATUS TO INFLUXDB ##
+#######################################
+
+if [[ ${INFLUXDB_ENABLED} == "true" ]]; then
+    if [[ $newipv4 =~ $regexv4 ]]; then
+        curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname) ipv4_status=1"
+    else
+        curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname) ipv4_status=0"
+    fi
+
+    if [[ $newipv6 =~ $regexv6 ]]; then
+        curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname) ipv6_status=1"
+    else
+        curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname) ipv6_status=0"
+    fi
+fi
+
+####################
+## UPDATE DOMAINS ##
+####################
 
 for index in ${!cfzone[*]}; do
 
@@ -29,43 +85,17 @@ for index in ${!cfzone[*]}; do
 
     case "${cftype[$index]}" in
         A)
-            regex='^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'
-            mode=4
-            record=A
+            regex="${regexv4}"
+            newip="${newipv4}"
             ;;
         AAAA)
-            regex='^([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}$'
-            mode=6
-            record=AAAA
-            ;;
-    esac
-
-    case "${DETECTION_MODE}" in
-        dig-google.com)
-            newip=$(dig -${mode} TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
-            ;;
-        dig-opendns.com)
-            newip=$(dig -${mode} ${record} +short myip.opendns.com @resolver1.opendns.com)
-            ;;
-        curl-icanhazip.com)
-            newip=$(curl -s -${mode} icanhazip.com)
-            ;;
-        curl-wtfismyip.com)
-            newip=$(curl -s -${mode} wtfismyip.com/text)
-            ;;
-        curl-showmyip.ca)
-            newip=$(curl -s -${mode} showmyip.ca/ip.php)
-            ;;
-        curl-da.gd)
-            newip=$(curl -s -${mode} da.gd/ip)
-            ;;
-        curl-seeip.org)
-            newip=$(curl -s -${mode} ip.seeip.org)
+            regex="${regexv6}"
+            newip="${newipv6}"
             ;;
     esac
 
     if ! [[ $newip =~ $regex ]]; then
-        echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Returned current IP is not valid! Check your connection." >> "${LOG}"
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Returned IP by \"${DETECTION_MODE}\" is not valid! Check your connection." >> "${LOG}"
     else
         if [[ ! -f "$cache" ]]; then
             zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[] | select (.name == "'"${cfzone[$index]}"'") | .id')
