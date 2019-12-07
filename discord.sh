ab0825ace40b1364566e0a72203a7ea40502d49c
hotio <hotio@protonmail.com>
2019-12-07 12:14:24 +0100

    [CI SKIP]

--- discord.sh
+++ discord.sh
@@ -9,4 +9,4 @@ else
 fi
 
 curl -fsSL -H "Content-Type: application/json" \
--X POST -d '{"embeds":[{"description":"Build **[#'"${DRONE_BUILD_NUMBER}"'](https://cloud.drone.io/'"${DRONE_REPO}"'/'"${DRONE_BUILD_NUMBER}"')** of `'"${DRONE_REPO//docker-/}"':'"${DRONE_COMMIT_BRANCH}"'` '"${message}"'.\n\nCommit **['"${DRONE_COMMIT_SHA:0:7}"']('"${DRONE_COMMIT_LINK}"')** by **'"${DRONE_COMMIT_AUTHOR}"'**:\n```'"${DRONE_COMMIT_MESSAGE}"'```","color":'"${color}"'}]}' "https://discordapp.com/api/webhooks/${DISCORD_ID}/${DISCORD_TOKEN}"
+-X POST -d '{"embeds":[{"description":"Build **[#'"${DRONE_BUILD_NUMBER}"'](https://cloud.drone.io/'"${DRONE_REPO}"'/'"${DRONE_BUILD_NUMBER}"')** of `'"${DRONE_REPO//docker-/}"':'"${DRONE_COMMIT_BRANCH}"'` '"${message}"'.\n\nCommit **['"${DRONE_COMMIT_SHA:0:7}"']('"${DRONE_COMMIT_LINK}"')** by **'"${DRONE_COMMIT_AUTHOR}"'**:\n```'"${DRONE_COMMIT_MESSAGE//\"/}"'```","color":'"${color}"'}]}' "https://discordapp.com/api/webhooks/${DISCORD_ID}/${DISCORD_TOKEN}"
