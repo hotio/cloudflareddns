bf9a0352be017ae5ca490a8f3c40d20b441b71f5
hotio <hotio@protonmail.com>
2019-12-22 23:22:13 +0100

    [CI SKIP]

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [[ ${1} == "checkpackages" ]]; then
-    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/base:stable-linux-arm64 bash -c 'apt list --installed > /github/upstream_packages.arm64.txt'
-    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/base:stable-linux-arm   bash -c 'apt list --installed > /github/upstream_packages.arm.txt'
-    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/base:stable-linux-amd64 bash -c 'apt list --installed > /github/upstream_packages.amd64.txt'
+if [[ ${1} == "checkdigests" ]]; then
+    docker pull hotio/base:stable-linux-arm64
+    docker pull hotio/base:stable-linux-arm
+    docker pull hotio/base:stable-linux-amd64
+    docker inspect --format='{{index .RepoDigests 0}}' hotio/base:stable-linux-arm64 >  upstream_digests.txt
+    docker inspect --format='{{index .RepoDigests 0}}' hotio/base:stable-linux-arm   >> upstream_digests.txt
+    docker inspect --format='{{index .RepoDigests 0}}' hotio/base:stable-linux-amd64 >> upstream_digests.txt
 fi
