6fdcbdfb4f51780bf378ae4fdc59351df531ae2d
hotio <hotio@protonmail.com>
2019-12-24 08:52:55 +0100

    [CI SKIP]

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 if [[ ${1} == "checkdigests" ]]; then
-    image="hotio/base:stable-linux-amd64" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*\$#FROM ${digest}#g" ./linux-amd64.Dockerfile
-    image="hotio/base:stable-linux-arm"   && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*\$#FROM ${digest}#g" ./linux-arm.Dockerfile
-    image="hotio/base:stable-linux-arm64" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*\$#FROM ${digest}#g" ./linux-arm64.Dockerfile
+    mkdir ~/.docker && echo '{"experimental": "enabled"}' > ~/.docker/config.json
+    image="hotio/base"
+    tag="latest"
+    manifest=$(docker manifest inspect ${image}:${tag})
+    [[ -z ${manifest} ]] && exit 1
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-arm.Dockerfile   && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
 fi
