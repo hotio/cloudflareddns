22acbd3f7486a2dab66da443cb52cb73fb051bf3
hotio <hotio@protonmail.com>
2020-01-22 20:52:28 +0100

    update

--- root/app/cloudflare-ddns.sh
+++ root/app/cloudflare-ddns.sh
@@ -8,16 +8,16 @@ echo $$ > /dev/shm/cloudflare-ddns.pid
 ###################################
 
 if [[ ${INFLUXDB_ENABLED} == "true" ]]; then
-    if result=$(curl -s -XPOST "${INFLUXDB_HOST}/query" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-urlencode "q=SHOW DATABASES"); then
+    if result=$(curl -fsSL -XPOST "${INFLUXDB_HOST}/query" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-urlencode "q=SHOW DATABASES"); then
         [[ ${LOG_LEVEL} -gt 0 ]] && echo "Connection to \"${INFLUXDB_HOST}\" succeeded!"
         if echo "${result}" | jq -erc ".results[].series[].values[] | select(. == [\"${INFLUXDB_DB}\"])" > /dev/null; then
             [[ ${LOG_LEVEL} -gt 0 ]] && echo "Database \"${INFLUXDB_DB}\" found!"
         else
             [[ ${LOG_LEVEL} -gt 0 ]] && echo "Database \"${INFLUXDB_DB}\" not found! Creating database..."
-            curl -s -XPOST "${INFLUXDB_HOST}/query" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-urlencode "q=CREATE DATABASE ${INFLUXDB_DB}" > /dev/null
+            curl -fsSL -XPOST "${INFLUXDB_HOST}/query" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-urlencode "q=CREATE DATABASE ${INFLUXDB_DB}" > /dev/null
             [[ ${LOG_LEVEL} -gt 0 ]] && echo "Adding sample data..."
-            curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "domains,host=sample-generator,domain=ipv4.cloudflare.com,recordtype=A ip=\"1.1.1.1\""
-            curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "domains,host=sample-generator,domain=ipv6.cloudflare.com,recordtype=AAAA ip=\"2606:4700:4700::1111\""
+            curl -fsSL -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "domains,host=sample-generator,domain=ipv4.cloudflare.com,recordtype=A ip=\"1.1.1.1\""
+            curl -fsSL -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "domains,host=sample-generator,domain=ipv6.cloudflare.com,recordtype=AAAA ip=\"2606:4700:4700::1111\""
         fi
     fi
 fi
@@ -57,39 +57,39 @@ while true; do
             newipv6=$(dig -6 AAAA +short myip.opendns.com @resolver1.opendns.com)
             ;;
         curl-icanhazip.com)
-            newipv4=$(curl -s -4 icanhazip.com)
-            newipv6=$(curl -s -6 icanhazip.com)
+            newipv4=$(curl -fsSL -4 icanhazip.com)
+            newipv6=$(curl -fsSL -6 icanhazip.com)
             ;;
         curl-wtfismyip.com)
-            newipv4=$(curl -s -4 wtfismyip.com/text)
-            newipv6=$(curl -s -6 wtfismyip.com/text)
+            newipv4=$(curl -fsSL -4 wtfismyip.com/text)
+            newipv6=$(curl -fsSL -6 wtfismyip.com/text)
             ;;
         curl-showmyip.ca)
-            newipv4=$(curl -s -4 showmyip.ca/ip.php)
-            newipv6=$(curl -s -6 showmyip.ca/ip.php)
+            newipv4=$(curl -fsSL -4 showmyip.ca/ip.php)
+            newipv6=$(curl -fsSL -6 showmyip.ca/ip.php)
             ;;
         curl-da.gd)
-            newipv4=$(curl -s -4 da.gd/ip)
-            newipv6=$(curl -s -6 da.gd/ip)
+            newipv4=$(curl -fsSL -4 da.gd/ip)
+            newipv6=$(curl -fsSL -6 da.gd/ip)
             ;;
         curl-seeip.org)
-            newipv4=$(curl -s -4 ip.seeip.org)
-            newipv6=$(curl -s -6 ip.seeip.org)
+            newipv4=$(curl -fsSL -4 ip.seeip.org)
+            newipv6=$(curl -fsSL -6 ip.seeip.org)
             ;;
     esac
 
     ## LOG CONNECTION STATUS TO INFLUXDB IF ENABLED ##
     if [[ ${INFLUXDB_ENABLED} == "true" ]]; then
         if [[ $newipv4 =~ $regexv4 ]]; then
-            curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv4 status=1,ip=\"$newipv4\""
+            curl -fsSL -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv4 status=1,ip=\"$newipv4\""
         else
-            curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv4 status=0,ip=\"no ipv4\""
+            curl -fsSL -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv4 status=0,ip=\"no ipv4\""
         fi
 
         if [[ $newipv6 =~ $regexv6 ]]; then
-            curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv6 status=1,ip=\"$newipv6\""
+            curl -fsSL -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv6 status=1,ip=\"$newipv6\""
         else
-            curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv6 status=0,ip=\"no ipv6\""
+            curl -fsSL -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "connection,host=$(hostname),type=ipv6 status=0,ip=\"no ipv6\""
         fi
     fi
 
@@ -113,40 +113,52 @@ while true; do
             [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Returned IP by \"${DETECTION_MODE}\" is not valid! Check your connection."
         else
             if [[ ! -f "$cache" ]]; then
+                zoneid=""
+                dnsrecords=""
                 if [[ -z ${cfapitoken} ]]; then
-                    zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[] | select (.name == "'"${cfzone[$index]}"'") | .id')
-                    dnsrecords=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[] | {name, id, zone_id, zone_name, content, type, proxied} | select (.name == "'"${cfhost[$index]}"'") | select (.type == "'"${cftype[$index]}"'")')
+                    response=$(curl -fsSL -X GET "https://api.cloudflare.com/client/v4/zones" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json") && \
+                        zoneid=$(echo "${response}" | jq -r '.result[] | select (.name == "'"${cfzone[$index]}"'") | .id') && \
+                        response=$(curl -fsSL -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json") && \
+                        dnsrecords=$(echo "${response}" | jq -r '.result[] | {name, id, zone_id, zone_name, content, type, proxied} | select (.name == "'"${cfhost[$index]}"'") | select (.type == "'"${cftype[$index]}"'")') && \
+                        echo "$dnsrecords" > "$cache"
                 else
-                    zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones" -H "Authorization: Bearer $cfapitoken" -H "Content-Type: application/json" | jq -r '.result[] | select (.name == "'"${cfzone[$index]}"'") | .id')
-                    dnsrecords=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" -H "Authorization: Bearer $cfapitoken" -H "Content-Type: application/json" | jq -r '.result[] | {name, id, zone_id, zone_name, content, type, proxied} | select (.name == "'"${cfhost[$index]}"'") | select (.type == "'"${cftype[$index]}"'")')
+                    response=$(curl -fsSL -X GET "https://api.cloudflare.com/client/v4/zones" -H "Authorization: Bearer $cfapitoken" -H "Content-Type: application/json") && \
+                        zoneid=$(echo "${response}" | jq -r '.result[] | select (.name == "'"${cfzone[$index]}"'") | .id') && \
+                        response=$(curl -fsSL -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" -H "Authorization: Bearer $cfapitoken" -H "Content-Type: application/json") && \
+                        dnsrecords=$(echo "${response}" | jq -r '.result[] | {name, id, zone_id, zone_name, content, type, proxied} | select (.name == "'"${cfhost[$index]}"'") | select (.type == "'"${cftype[$index]}"'")') && \
+                        echo "$dnsrecords" > "$cache"
                 fi
-                echo "$dnsrecords" > "$cache"
             else
                 dnsrecords=$(cat "$cache")
                 zoneid=$(echo "$dnsrecords" | jq -r '.zone_id' | head -1)
             fi
-            id=$(echo "$dnsrecords" | jq -r '.id' | head -1)
-            proxied=$(echo "$dnsrecords" | jq -r '.proxied' | head -1)
-            ip=$(echo "$dnsrecords" | jq -r '.content' | head -1)
-            if ! [[ $ip =~ $regex ]]; then
-                [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Returned IP by \"Cloudflare\" is not valid! Check your connection or configuration."
-            else
-                if [[ "$ip" != "$newip" ]]; then
-                    if [[ -z ${cfapitoken} ]]; then
-                        response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" --data '{"id":"'"$id"'","type":"'"${cftype[$index]}"'","name":"'"${cfhost[$index]}"'","content":"'"$newip"'","proxied":'"$proxied"'}' | jq '.success')
-                    else
-                        response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" -H "Authorization: Bearer $cfapitoken" -H "Content-Type: application/json" --data '{"id":"'"$id"'","type":"'"${cftype[$index]}"'","name":"'"${cfhost[$index]}"'","content":"'"$newip"'","proxied":'"$proxied"'}' | jq '.success')
-                    fi
-                    if [[ ${response} == true ]]; then
-                        [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Updating IP [$ip] to [$newip]: OK"
-                        [[ ${INFLUXDB_ENABLED} == "true" ]] && curl -s -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "domains,host=$(hostname),domain=${cfhost[$index]},recordtype=${cftype[$index]} ip=\"$newip\""
-                        rm "$cache"
-                    else
-                        [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Updating IP [$ip] to [$newip]: FAILED"
-                    fi
+            if [[ -n ${dnsrecords} ]]; then
+                id=$(echo "$dnsrecords" | jq -r '.id' | head -1)
+                proxied=$(echo "$dnsrecords" | jq -r '.proxied' | head -1)
+                ip=$(echo "$dnsrecords" | jq -r '.content' | head -1)
+                if ! [[ $ip =~ $regex ]]; then
+                    [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Returned IP by \"Cloudflare\" is not valid! Check your connection or configuration."
                 else
-                    [[ ${LOG_LEVEL} -gt 1 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Updating IP [$ip] to [$newip]: NO CHANGE"
+                    if [[ "$ip" != "$newip" ]]; then
+                        result=NOK
+                        if [[ -z ${cfapitoken} ]]; then
+                            curl -fsSL -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" --data '{"id":"'"$id"'","type":"'"${cftype[$index]}"'","name":"'"${cfhost[$index]}"'","content":"'"$newip"'","proxied":'"$proxied"'}' && result=OK
+                        else
+                            curl -fsSL -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" -H "Authorization: Bearer $cfapitoken" -H "Content-Type: application/json" --data '{"id":"'"$id"'","type":"'"${cftype[$index]}"'","name":"'"${cfhost[$index]}"'","content":"'"$newip"'","proxied":'"$proxied"'}' && result=OK
+                        fi
+                        if [[ ${result} == OK ]]; then
+                            [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Updating IP [$ip] to [$newip]: OK"
+                            [[ ${INFLUXDB_ENABLED} == "true" ]] && curl -fsSL -XPOST "${INFLUXDB_HOST}/write?db=${INFLUXDB_DB}" -u "${INFLUXDB_USER}:${INFLUXDB_PASS}" --data-binary "domains,host=$(hostname),domain=${cfhost[$index]},recordtype=${cftype[$index]} ip=\"$newip\""
+                            rm "$cache"
+                        else
+                            [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Updating IP [$ip] to [$newip]: FAILED"
+                        fi
+                    else
+                        [[ ${LOG_LEVEL} -gt 1 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Updating IP [$ip] to [$newip]: NO CHANGE"
+                    fi
                 fi
+            else
+                [[ ${LOG_LEVEL} -gt 0 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Couldn't fetch DNS records from \"Cloudflare\"! Check your connection or configuration."
             fi
         fi
 
