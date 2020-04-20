9307dddc37f29402b94e54d89fc58289122d3bb7
hotio <26902309+hotio@users.noreply.github.com>
2020-04-20 16:54:32 +0200

    update

--- root/app/cloudflare-ddns.sh
+++ root/app/cloudflare-ddns.sh
@@ -120,14 +120,14 @@ while true; do
 
         curl_header() {
             if [[ -n $cfapitokenzone ]] && [[ $* != *dns_records* ]]; then
+                [[ ${LOG_LEVEL} -gt 2 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - Using \"CF_APITOKEN_ZONE=$cfapitokenzone\" to authenticate..."
                 curl -fsSL -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $cfapitokenzone" "$@"
-                [[ ${LOG_LEVEL} -gt 2 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Used \"CF_APITOKEN_ZONE=$cfapitokenzone\" to authenticate..."
             elif [[ -n $cfapitoken ]]; then
+                [[ ${LOG_LEVEL} -gt 2 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - Using \"CF_APITOKEN=$cfapitoken\" to authenticate..."
                 curl -fsSL -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $cfapitoken" "$@"
-                [[ ${LOG_LEVEL} -gt 2 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Used \"CF_APITOKEN=$cfapitoken\" to authenticate..."
             else
+                [[ ${LOG_LEVEL} -gt 2 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - Using \"CF_USER=$cfuser, CF_APIKEY=$cfapikey\" to authenticate..."
                 curl -fsSL -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Auth-Email: $cfuser" -H "X-Auth-Key: $cfapikey" "$@"
-                [[ ${LOG_LEVEL} -gt 2 ]] && echo "$(date +'%Y-%m-%d %H:%M:%S') - [${DETECTION_MODE}] - [${cfhost[$index]}] - [${cftype[$index]}] - Used \"CF_USER=$cfuser, CF_APIKEY=$cfapikey\" to authenticate..."
             fi
         }
 
