b394541361065e05754e4bad5532c249b5a15786
hotio <26902309+hotio@users.noreply.github.com>
2020-08-08 18:26:38 +0200

    switch to gha

--- linux-amd64.Dockerfile
+++ linux-amd64.Dockerfile
@@ -10,3 +10,16 @@ RUN apk add --no-cache python3 py3-six py3-requests py3-pip ncurses iproute2 bin
 COPY root/ /
 
 RUN chmod 755 "${APP_DIR}/cloudflare-ddns.sh"
+
+ARG LABEL_CREATED
+LABEL org.opencontainers.image.created=$LABEL_CREATED
+ARG LABEL_TITLE
+LABEL org.opencontainers.image.title=$LABEL_TITLE
+ARG LABEL_REVISION
+LABEL org.opencontainers.image.revision=$LABEL_REVISION
+ARG LABEL_SOURCE
+LABEL org.opencontainers.image.source=$LABEL_SOURCE
+ARG LABEL_VENDOR
+LABEL org.opencontainers.image.vendor=$LABEL_VENDOR
+ARG LABEL_URL
+LABEL org.opencontainers.image.url=$LABEL_URL
--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@ if [[ ${1} == "checkdigests" ]]; then
     tag="alpine"
     manifest=$(docker manifest inspect ${image}:${tag})
     [[ -z ${manifest} ]] && exit 1
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm.Dockerfile   && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
 else
     version_apprise=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version_apprise} ]] && exit 1
-    sed -i "s/{APPRISE_VERSION=[^}]*}/{APPRISE_VERSION=${version_apprise}}/g" .drone.yml
+    sed -i "s/{APPRISE_VERSION=[^}]*}/{APPRISE_VERSION=${version_apprise}}/g" .github/workflows/build.yml
     version="${version_apprise}"
     echo "##[set-output name=version;]${version}"
 fi
