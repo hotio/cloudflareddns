e2669fa2d9227682691276b8c92034104fe0e46b
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 15:21:39 +0200

    rework build

--- update.sh
+++ update.sh
@@ -9,10 +9,15 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    echo "Show apprise version info..."
+    docker run --rm --entrypoint="" "${2}" apprise --version
 else
     version_apprise=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version_apprise} ]] && exit 1
-    sed -i "s/{APPRISE_VERSION=[^}]*}/{APPRISE_VERSION=${version_apprise}}/g" .github/workflows/build.yml
+    echo "APPRISE_VERSION=${version_apprise}" > VERSION
     version="${version_apprise}"
     echo "##[set-output name=version;]${version}"
 fi
