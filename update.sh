5207f699fe457f4627fef4fc16595d72d5873dc0
hotio <26902309+hotio@users.noreply.github.com>
2020-11-02 09:00:09 +0100

    fix check

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ ${1} == "checkdigests" ]]; then
-    mkdir ~/.docker && echo '{"experimental": "enabled"}' > ~/.docker/config.json
+    export DOCKER_CLI_EXPERIMENTAL=enabled
     image="hotio/base"
     tag="alpine"
     manifest=$(docker manifest inspect ${image}:${tag})
     [[ -z ${manifest} ]] && exit 1
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux" and .platform.variant == "v7").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
 elif [[ ${1} == "tests" ]]; then
     echo "List installed packages..."
