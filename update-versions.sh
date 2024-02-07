9ad69bb3b42fbb99dd21930eeb829aac8dd48113
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 08:13:37 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-apprise_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${apprise_version} ]] && exit 0
+set -e
+apprise_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -re .tag_name)
 json=$(cat VERSION.json)
 jq --sort-keys \
-    --arg apprise_version "${apprise_version}" \
-    '.apprise_version = $apprise_version' <<< "${json}" > VERSION.json
+    --arg apprise_version "${apprise_version//v/}" \
+    '.apprise_version = $apprise_version' <<< "${json}" | tee VERSION.json
