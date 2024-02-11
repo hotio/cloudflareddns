2b86a6818568714d862ab68770dbc4e48e2595cb
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-11 12:18:03 +0100

    [skip ci]

--- update-self-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if ! git diff --quiet; then
-    json=$(cat VERSION.json)
-    jq --sort-keys \
-        --arg version "$(date -u +'%Y%m%d%H%M%S')" \
-        '.version = $version' <<< "${json}" | tee VERSION.json
-fi
--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apprise_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -re .tag_name) || exit 1
+version=$(git hash-object ./root/app/cloudflare-ddns.sh)
 json=$(cat VERSION.json)
 jq --sort-keys \
+    --arg version "${version:0:7}--${apprise_version//v/}" \
     --arg apprise_version "${apprise_version//v/}" \
-    '.apprise_version = $apprise_version' <<< "${json}" | tee VERSION.json
+    '.version = $version | .apprise_version = $apprise_version' <<< "${json}" | tee VERSION.json
