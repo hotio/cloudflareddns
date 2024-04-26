51007fab29c0ec860562579b3a7411e70406c5df
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-04-26 06:59:16 +0200

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apprise_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -re .tag_name) || exit 1
+[[ -z ${apprise_version} ]] && exit 0
+[[ ${apprise_version} == null ]] && exit 0
 version=$(git hash-object ./root/app/cloudflare-ddns.sh)
 json=$(cat VERSION.json)
 jq --sort-keys \
