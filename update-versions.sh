6fe9de0204b6b1a5b3396cf6fb2151dd80e37d61
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 07:57:49 +0100

    remove version field

--- update-versions.sh
+++ update-versions.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-apprise_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -re .tag_name) || exit 1
-[[ -z ${apprise_version} ]] && exit 0
-[[ ${apprise_version} == null ]] && exit 0
-version=$(git hash-object ./root/app/cloudflare-ddns.sh)
+set -euo pipefail
+
+apprise_version=$(curl -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -re .tag_name)
 json=$(cat VERSION.json)
 jq --sort-keys \
-    --arg version "${version:0:7}--${apprise_version//v/}" \
     --arg apprise_version "${apprise_version//v/}" \
-    '.version = $version | .apprise_version = $apprise_version' <<< "${json}" | tee VERSION.json
+    '.apprise_version = $apprise_version' <<< "${json}" | tee VERSION.json
