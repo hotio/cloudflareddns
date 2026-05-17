f2a431fa2af30f05d31454ff587fa0ca34a43ed5
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 16:18:34 +0200

    add __command [skip ci]

--- update-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-version_apprise=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -re .tag_name)
-json=$(cat meta.json)
-jq --sort-keys \
-    --arg version_apprise "${version_apprise//v/}" \
-    '.version_apprise = $version_apprise' <<< "${json}" | tee meta.json
